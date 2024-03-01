113cb6ad5b19040f772b04263543580345972e49
Joshua Matthews <joshua.matthews@patracorp.com>
2024-03-01 12:24:46 -0600

    added file functions, tracker module, and initial procedure module

--- lua/time-tracker.nvim/file.lua
+++ lua/time-tracker.nvim/file.lua
@@ -1,5 +1,6 @@
-
 local M = {}
+
+local tracker_file = "time-tracker.nvim.json"
 
 local function native_separator()
     if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1 then
@@ -12,11 +13,34 @@ M.data_path = vim.fn.stdpath("data")
 M.path_sep = native_separator()
 
 function M.grab_or_create_file()
-    local f = io.open(M.data_path .. M.path_sep .. "tmp_file", "r+")
+    local f = io.open(M.data_path .. M.path_sep .. tracker_file, "w+")
     if f ~= nil then
         return f
     else
         vim.notify("could not open data file for nvim-time-tracker", vim.log.levels.ERROR)
+    end
+end
+
+function M.write_to_tracker_file(obj)
+    local data = vim.json.encode(obj)
+    local f = M.grab_or_create_file()
+    if f ~= nil then
+        f:write(data)
+        f:close()
+    end
+end
+
+function M.rend_tracker_file()
+    local f = M.grab_or_create_file()
+    if f ~= nil then
+        local data = f:read("*a")
+        -- decode file and change JSON nulls to Lua's nils for objects and arrays
+        local obj = nil
+        if data ~= "" then
+            obj = vim.json.decode(data, {object = true, array = true})
+        end
+        f:close()
+        return obj
     end
 end
 
