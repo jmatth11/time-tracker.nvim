0f002a91664b1c94892986814c04e6a467e25496
Joshua Matthews <joshua.matthews@patracorp.com>
2024-03-02 11:38:31 -0600

    small changes

--- lua/time-tracker/init.lua
+++ lua/time-tracker/init.lua
@@ -5,17 +5,24 @@ local M = {
 }
 local main_proc = proc:new()
 
-vim.on_key(function()
-    main_proc:debounce_timer()
-end,
-M.nid)
-
 function M.time_info()
     print(vim.inspect(main_proc:time_info()))
 end
 
 function M.update()
     main_proc:update()
+end
+
+function M.setup(opts)
+    if opts ~= nil then
+        if opts["timer_delay"] ~= nil then
+            main_proc.timer_delay = opts.timer_delay
+        end
+    end
+    vim.on_key(function()
+        main_proc:debounce_timer()
+    end,
+    M.nid)
 end
 
 return M
