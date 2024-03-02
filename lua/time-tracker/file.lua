286a2f4507368fd2fc0305c48d039c9aed50b23c
Joshua Matthews <joshua.matthews@patracorp.com>
2024-03-02 12:27:53 -0600

    documented and cleaned up some thing

--- lua/time-tracker/file.lua
+++ lua/time-tracker/file.lua
@@ -1,7 +1,9 @@
 local M = {}
 
+-- tracker file for persisting user data
 local tracker_file = "time-tracker.nvim.json"
 
+-- Get the native separate for joining paths
 local function native_separator()
     if vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1 then
         return "\\"
@@ -9,9 +11,12 @@ local function native_separator()
     return "/"
 end
 
+-- grab the nvim user data directory
 M.data_path = vim.fn.stdpath("data")
 M.path_sep = native_separator()
 
+-- Grab or creeate the user data file.
+-- @returns File object
 function M.grab_or_create_file()
     local f = io.open(M.data_path .. M.path_sep .. tracker_file, "r+")
     if f ~= nil then
@@ -21,6 +26,7 @@ function M.grab_or_create_file()
     end
 end
 
+-- Write the given object to the tracker file.
 function M.write_to_tracker_file(obj)
     local data = vim.json.encode(obj)
     local f = M.grab_or_create_file()
@@ -30,12 +36,15 @@ function M.write_to_tracker_file(obj)
     end
 end
 
+-- Read the user data from the tracker file.
+-- @returns Table
 function M.read_tracker_file()
     local f = M.grab_or_create_file()
     if f ~= nil then
         local data = f:read("*a")
         -- decode file and change JSON nulls to Lua's nils for objects and arrays
         local obj = nil
+        -- data will be empty on initial file creation
         if data ~= "" then
             obj = vim.json.decode(data, {object = true, array = true})
         end
--- lua/time-tracker/init.lua
+++ lua/time-tracker/init.lua
@@ -8,24 +8,31 @@ local M = {
 }
 local main_proc = proc:new()
 
-function M.time_info()
+-- Inspect the time info for debug purposes.
+function M.time_info_debug()
     print(vim.inspect(main_proc:time_info()))
 end
 
+-- Force an update
 function M.update()
     main_proc:update()
 end
 
+-- Setup the main functionality of tracking.
+-- @param opts This is a table of options.
+--      opt["timer_delay"] - customize how long the delay before setting inactive status.
 function M.setup(opts)
     if opts ~= nil then
         if opts["timer_delay"] ~= nil then
             main_proc.timer_delay = opts.timer_delay
         end
     end
+    -- listen for every key press so we can debounce the update function
     vim.on_key(function()
         main_proc:debounce_timer()
     end,
     M.nid)
+    -- create our autocommand to save off data if we leave vim
     vim.api.nvim_create_augroup(autogroup, {clear = true})
     vim.api.nvim_create_autocmd("VimLeavePre", {
         group = autogroup,
