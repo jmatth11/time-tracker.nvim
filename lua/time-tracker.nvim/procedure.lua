6e04fca4d61b39a663ce146a740a96ff628a30cc
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:23:51 -0600

    created procedure module to handle debouncing of active state

--- lua/time-tracker.nvim/procedure.lua
+++ lua/time-tracker.nvim/procedure.lua
@@ -1,4 +1,23 @@
+local tracker = require("time-tracker.nvim.tracker")
 local M = {}
 
+-- timer delay set to 5 minute
+local timer_delay = 1000 * 60 * 5
+
+function M:new(o)
+    local obj = o or { timer = nil, timer_delay = timer_delay }
+    self.__index = self
+    return setmetatable(obj, self)
+end
+
+function M:debounce_timer()
+    -- check if timer exists
+    if self.timer ~= nil then
+        -- cancel timer
+        self.timer:stop()
+    end
+    -- (re)create and start timer
+    self.timer = vim.defer_fn(tracker.set_inactive(), self.timer_delay)
+end
 
 return M
